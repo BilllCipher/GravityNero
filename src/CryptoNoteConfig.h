aff0717b17259754624177fe545d09de40f979c7
Billl_Cipher <113240932+BilllCipher@users.noreply.github.com>
2023-12-05 20:20:07 +1100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "3.26.15.186:9901",
-  "3.106.198.75:9901",
+  "3.27.44.253:9901",
 };
 
 struct CheckpointData {
